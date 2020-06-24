128b6bc43d6974f476c07c9784213ff9ef058394
Tonow-c2c <thomas.nowicki@camptocamp.com>
2020-06-24 17:12:40 +0200

    [IMP][sale_order_line_packaging_qty] Sell product / unit or packaging
    *Context:*
    
    We want to be able to only sell by packaging some product (forbidden to
     sell by unit). Also, we want to choose which packaging is eligible to
     sell.
    
    *Usage*
    
    When I encode a sale order line, I must fill up the packaging and
    related quantity if the product is set as "Only sell by packaging".
    I cannot enter a quantity in the standard Odoo field quantity that
    is not a multiple of a sellable packaging
    (product.packaging can be sold = True).
    
    Is the product is not configured as "Only sell by packaging",
    then I am allowed to fill up a line with no packaging.
    
    On every packaging I can chose if it is sellable or not. By default it is.
    
    I can chose at product level if i restrict the quantity to sell
    to existing sellable packaging.
    
    *Dev*
    
    In the exiting OCA module sale_order_line_packaging_qty:
    * Add a check box in product "Only sell by packaging multiple" (default False), aside of "Can be sold" in the header
    * Add a boolean on the packaging "Can be sold" (default True)
    * On the list of available packaging in a SO line, only display packaging with "can be sold = True"
    * When a SO line is encoded *with a product that can only be sold by packaging*:
        * The quantity field cannot contain a quantity that is not a multiple of a sellable packaging (can be sold = True) -> raise a warning (triggered on the on_change please, not create/write as we still want to be able to save it e.g. in case of an EDI order) -> "The quantity is not a multiple of a defined sellable packaging. Please complete the packaging first."
        * On create / write method, *if packaging and packaging quantity is empty*, fulfill the info if a multiple of an existing sellable packaging (can be sold = True) is found
            * e.g. if my product is sold by packaging only and I have packaging of box: 10, Karton: 200, and pallet:1000, if the quantity entered is 400, I expect the system to complete the packaging with Karton and packaging quantity with 2
            * e.g. if my product is sold by packaging only and I have packaging of box: 10, Karton: 200, and pallet:1000, if the quantity entered is 420, I expect the system to complete the packaging with box and packaging quantity with 42
            * e.g.. if my product is sold by packaging only and I have packaging of box: 10, Karton: 200, and pallet:1000, if the quantity entered is 405, I expect the system to leave it empty
        * Make the message "You must define a package before setting a quantity of said package." a user warning and not a blocking point AND only raise this warning for product that have "Only sell by packaging multiple = True"

--- sale_order_line_packaging_qty/tests/test_sale_order_line_packaging_qty.py
+++ sale_order_line_packaging_qty/tests/test_sale_order_line_packaging_qty.py
@@ -30,3 +30,58 @@ class TestSaleOrderLinePackagingQty(SavepointCase):
         self.assertEqual(order_line.product_packaging_qty, 1.0)
         order_line.write({"product_packaging_qty": 3.0})
         self.assertEqual(order_line.product_uom_qty, 15.0)
+
+    def test_onchange_qty_is_pack_multiple(self):
+        order = self.env["sale.order"].create({"partner_id": self.partner.id})
+        order_line = self.env["sale.order.line"].create(
+            {
+                "order_id": order.id,
+                "product_id": self.product.id,
+                "product_uom": self.product.uom_id.id,
+            }
+        )
+        self.assertFalse(order_line._onchange_product_uom_qty())
+
+        self.product.write({"only_sell_by_pack": True})
+        self.assertTrue(order_line._onchange_product_uom_qty())
+
+        order_line.product_id_change()
+        self.assertFalse(order_line._onchange_product_uom_qty())
+
+        order_line.write({"product_uom_qty": 3.0})
+        self.assertTrue(order_line._onchange_product_uom_qty())
+
+        order_line.write({"product_uom_qty": self.packaging.qty * 2})
+        self.assertFalse(order_line._onchange_product_uom_qty())
+
+    # def test_auto_fill_packaging(self):
+    #     order = self.env["sale.order"].create({"partner_id": self.partner.id})
+    #     packaging_10 = self.env["product.packaging"].create(
+    #         {"name": "Test packaging 10", "product_id": self.product.id, "qty": 10.0}
+    #     )
+    #     order_line = self.env["sale.order.line"].create(
+    #         {
+    #             "order_id": order.id,
+    #             "product_id": self.product.id,
+    #             "product_uom": self.product.uom_id.id,
+    #         }
+    #     )
+    #     self.assertFalse(order_line.product_packaging)
+    #     self.assertFalse(order_line.product_packaging_qty)
+    #
+    #     order_line.write({"product_uom_qty": 3.0})
+    #     self.assertFalse(order_line.product_packaging)
+    #     self.assertFalse(order_line.product_packaging_qty)
+    #
+    #     self.product.write({"only_sell_by_pack": True})
+    #     self.assertFalse(order_line.product_packaging)
+    #     self.assertFalse(order_line.product_packaging_qty)
+    #
+    #     order_line.write({"product_uom_qty": self.packaging.qty * 2})
+    #     self.assertTrue(order_line.product_packaging)
+    #     self.assertTrue(order_line.product_packaging_qty)
+    #     self.assertEqual(order_line.product_packaging.name, "Test packaging")
+    #     self.assertTrue(order_line.product_packaging_qty, 2)
+    #
+    #     order_line.write({"product_uom_qty": self.packaging.qty * 2})
+    #     self.assertEqual(order_line.product_packaging.name, "Test packaging 10")
